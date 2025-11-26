00a8074f9537b462c11c6aeb55e0d02ac6f9c007
Sahil Singh <sahilsinghgbss1@gmail.com>
2025-11-26 11:06:19 +0530

    web

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ use App\Http\Controllers\TrainerController;
 use App\Http\Controllers\OneDriveController;
 use App\Http\Controllers\StaffController;
 use App\Http\Controllers\TrainingEvidenceController;
-use App\Http\Controllers\TrainingUploadController;
 use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Support\Facades\Route;
 
@@ -23,15 +22,19 @@ Route::get('/', function () {
 });
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+    Route::get('/district-progress', [DashboardController::class, 'getDistrictProgress'])->name('district.progress');
+    
     Route::get('/chart-data', [DashboardController::class, 'getChartData'])->name('chart.data');
 
 });
 
-Route::middleware([RoleMiddleware::class . ':OKCL, OCAC, DLC'])->group(function () {
-    Route::get('/coordinatorlist', [CoordinatorController::class, 'index'])->name('coordinators.index');
-});
+
+Route::post('/uploadgallery', [DashboardController::class, 'photogallery'])->name('uploadgallery');
 
 Route::middleware(['auth', 'session.expired'])->group(function () {
+    Route::get('/fetchgallery', [DashboardController::class, 'fetchGallery'])->name('fetch.gallery');
+    Route::get('/preview-image', [DashboardController::class, 'previewImage'])->name('preview.image');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -42,16 +45,15 @@ Route::middleware(['auth', 'session.expired'])->group(function () {
     Route::get('/attendance-list/{id}/edit', [AttendanceController::class, 'edit'])->name('attendance.list.edit');
     Route::put('/attendance-list/{id}', [AttendanceController::class, 'update'])->name('attendance.list.update');
     Route::get('/preview-file', [AttendanceController::class, 'previewFile'])->name('preview.file');
-
-
+    
+    
     Route::get('/trainingphotos',[TrainingEvidenceController::class,'trainingphotos'])->name('trainingphotos');
     Route::post('/trainingphotos',[TrainingEvidenceController::class,'upload'])->name('upload.trainingphotos');
     Route::get('/trainingphotos-list',[TrainingEvidenceController::class,'trainingphotoslist'])->name('trainingphotos.list');
     Route::get('/trainingphotos-list/{id}/edit', [TrainingEvidenceController::class, 'editTrainingPhoto'])->name('training.photo.edit');
     Route::put('/trainingphotos-list/{id}', [TrainingEvidenceController::class, 'updateTrainingPhoto'])->name('training.photo.update');
     Route::get('/preview-image', [TrainingEvidenceController::class, 'previewImage'])->name('preview.image');
-    Route::get('/training-gallery', [DashboardController::class, 'gallery'])->name('gallery');
-
+    
     
     Route::get('/trainingvideos',[TrainingEvidenceController::class,'trainingvideos'])->name('trainingvideos');
     Route::post('/trainingvideos',[TrainingEvidenceController::class,'uploadvideo'])->name('upload.trainingvideos');
@@ -63,6 +65,8 @@ Route::middleware(['auth', 'session.expired'])->group(function () {
 
     Route::get('/trainingcompcertificate',[TrainingEvidenceController::class,'trainingcompcertificate'])->name('trainingcompcertificate');
     Route::post('/trainingcompcertificate',[TrainingEvidenceController::class,'uploadcertificate'])->name('upload.certificate');
+    Route::get('/uploaded-certificates', [TrainingEvidenceController::class, 'viewUploadedCertificates'])->name('uploaded.certificates');
+
     Route::get('/addstudent',[StudentController::class,'addstudent'])->name('addstudent');
     // Route::view('/addstudentsin','addstudentsin')->name('addstudentsin');
     
@@ -82,6 +86,16 @@ Route::middleware(['auth', 'session.expired'])->group(function () {
     // Delete student
     Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('student.delete');
     
+    Route::get('/student-feedback',[StudentController::class,'studentFeedback'])->name('student.feedback');
+    Route::post('/student-feedback', [StudentController::class, 'uploadFeedback'])->name('student.feedback.upload');
+    Route::get('/student-feedback-preview', [StudentController::class, 'previewFile'])->name('student.feedback.preview');
+    Route::get('/student-feedback-{stu_id}-entry', [StudentController::class, 'createEntry'])->name('student.feedback.entryPage');
+    Route::post('/student-feedback-store', [StudentController::class, 'storeStudentFeedback'])->name('student.feedback.store');
+    Route::post('/student-feedback-update', [StudentController::class, 'updateFeedback'])->name('student.feedback.update');
+    Route::get('/feedback-report', [StudentController::class, 'index'])->name('feedback.report');
+
+    Route::get('/get-schools-by-district-{districtId}', [StudentController::class, 'getSchoolsByDistrict']);
+
     Route::get('/feedback',[FeedbackController::class,'writtenfeedback'])->name('writtenfeedback');
     Route::post('/feedback',[FeedbackController::class,'uploadwrittenfeedback'])->name('upload.writtenfeedback');
 
@@ -91,19 +105,48 @@ Route::middleware(['auth', 'session.expired'])->group(function () {
     Route::get('/uploadfeedback-list/{id}/edit', [FeedbackController::class, 'editFeedbackVideo'])->name('feedback.video.edit');
     Route::put('/uploadfeedback-list/{id}', [FeedbackController::class, 'updatevideofeedback'])->name('feedback.video.update');
 
-    Route::get('/onlinefeedback',[FeedbackController::class,'onlinefeedback'])->name('onlinefeedback');
-
     Route::get('/uploadreport',[ReportController::class,'uploadreport'])->name('uploadreport');
 
-    Route::get('/uploadbills',[BillController::class,'uploadbills'])->name('uploadbills');
-    Route::get('/uploadtravelbills',[BillController::class,'uploadtravelbills'])->name('uploadtravelbills');
-    Route::get('/uploadexpensebills',[BillController::class,'uploadexpensebills'])->name('uploadexpensebills');
+    Route::get('/uploadstaffexpense',[BillController::class,'staffexpense'])->name('uploadbills');
+    Route::get('/get-staff/{schoolId}/{roleId}', [BillController::class, 'getStaff']);
+    Route::post('/staff-bill/store', [BillController::class, 'store'])->name('staff-bill.store');
     
-    Route::get('/schools', [SchoolController::class, 'index'])->name('index');
-    Route::get('/schools/get-by-dlc/{dlc_id}', [SchoolController::class, 'getByDlc'])->name('schools.getByDlc');
+    Route::get('/trainertravels',[BillController::class,'trainerTravels'])->name('trainer.travels');    
+    Route::get('/get-trainers/{district}/{specialization}', [BillController::class, 'getTrainersBySpecialization']);
+    Route::post('/trainertravels-store',[BillController::class,'trainerTravelStore'])->name('trainer.travel.store');
+    
+    Route::get('/trainer-travel-list', [BillController::class, 'trainerTravelList'])->name('trainer.travel.list');
+    Route::put('/trainer-travel/{id}', [BillController::class, 'trainerTravelUpdate'])->name('trainer.travel.update');
+    Route::post('/trainer-travel/{id}/update-training-date', [BillController::class, 'updateTrainingDate'])->name('trainerTravel.updateTrainingDate');
+
+    Route::get('/trainer-bill-preview', [BillController::class, 'previewFile'])->name('trainer.travel.preview');
+
+    Route::post('/trainer-travel/{id}/approve', [BillController::class, 'approve'])->name('trainerTravel.approve');
+    Route::post('/trainer-travel/{id}/reject', [BillController::class, 'reject'])->name('trainerTravel.reject');
+    Route::post('/trainer-travel/{id}/revert', [BillController::class, 'revert'])->name('trainerTravel.revert');
+
+
+    
+    Route::get('/uploadcampexpense',[BillController::class,'uploadcampexpense'])->name('uploadexpensebills');
+    
+    Route::get('/schools', [SchoolController::class, 'index'])->name('student.school');
+    Route::get('/district-{id}-schools', [SchoolController::class, 'getSchools']);
+    Route::get('/school-{id}-students', [SchoolController::class, 'getStudentsPage'])->name('school.students');
+    Route::get('/district-{id}-school-list', [SchoolController::class, 'districtSchoolList'])->name('district.school.list');
+    
+    Route::get('/district-list', [SchoolController::class, 'index'])->name('select.district');
+    Route::get('/district-{id}-list', [SchoolController::class, 'selectdistrictList'])->name('select.school');
+    Route::get('/district-{id}-schools', [SchoolController::class, 'getSchools']);
+    Route::get('/school/{id}/coordinators-json', [SchoolController::class, 'schoolCoordinatorsJson']);
+    Route::get('/school/{id}/trainers-json', [SchoolController::class, 'schoolTrainersJson']);
+    Route::get('/school/{id}/staffs-json', [SchoolController::class, 'schoolStaffsJson']);
+
+});
+Route::middleware([RoleMiddleware::class . ':OKCL,DLC'])->group(function () {
 });
 
-Route::middleware([RoleMiddleware::class . ':OCAC,OKCL,DLC'])->group(function () {
+
+Route::middleware([RoleMiddleware::class . ':OCAC,OKCL,DLC,Coordinator'])->group(function () {
     Route::get('/coordinatorlist', [CoordinatorController::class, 'index'])->name('coordinators.index');
     Route::post('/coordinators/store', [CoordinatorController::class, 'store'])->name('coordinators.store');
     Route::get('/coordinators/{coordinator}/edit', [CoordinatorController::class, 'edit'])->name('coordinators.edit');   
